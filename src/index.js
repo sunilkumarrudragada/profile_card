272f6200390ba1659e42257469e3c37986d79e8e
sunil7456 <sunilrudragada@gmail.com>
2025-07-10 15:30:24 +0530

    profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Avatar() {
+  return <img src="sunil.jpeg" className="avatar" alt="Sunil"></img>;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Rudragada Sunil Kumar</h1>
+      <p>Looking for new opportunites</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill color="orangered" skill="MERN" emoji="💪" />
+      <Skill color="green" skill="AI/ML" emoji="👶" />
+      <Skill color="yellow" skill="DEV-ops" emoji="👶" />
+      <Skill color="cyan" skill="React" emoji="👶" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
